5bda243c99907b58e563ef4a3ae5598ced6c8d91
cathy192 <cathy192@naver.com>
2019-06-08 13:15:01 +0900

    인기글 db 및 main css 수정

--- board/src/com/login/action/LoginAction.java
+++ board/src/com/login/action/LoginAction.java
@@ -65,7 +65,8 @@ public class LoginAction implements CommandAction{
     		if(conn != null) try{conn.close();} catch(SQLException ex){}
     		if(rs != null) try{rs.close();} catch(SQLException ex){}
     		}    	       	
-    	return "main.jsp";
+    	//return "main.jsp";
+    	return "best.do";
 	}
 	
 }
